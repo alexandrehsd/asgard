529ba2c5704d0eb56e5a8ed92328c0b3ddc5c248
Alexandre Henrique <alexandre.hsd@gmail.com>
2023-02-04 04:26:24 -0300

    Add util function to log the code

--- sdg_classifier/utils/monitor.py
+++ sdg_classifier/utils/monitor.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(name)s - %(levelname)s: %(message)s",
-    datefmt='%d-%m-%Y %H:%M:%S'
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 LOGGER = logging.getLogger()
