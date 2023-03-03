22f49c4fd5bfb0f83019255b7a8d539929c43d38
Alexandre Henrique <alexandre.hsd@gmail.com>
2023-03-02 23:20:19 -0300

    Add tests for balanced_multilabel_dataset function

--- tests/unit/test_loader.py
+++ tests/unit/test_loader.py
@@ -1,6 +1,11 @@
 import os
 import pandas as pd
-from asgard.dataset.loader import load_datasets, build_dataset, remove_duplicates
+from asgard.dataset.loader import (
+    load_datasets,
+    build_dataset,
+    remove_duplicates,
+    balance_multilabel_dataset
+)
 
 CSV_FILEPATH = "./tests/unit/mock_data"
 
@@ -27,4 +32,35 @@ def test_remove_duplicates():
 
     deduplicate_dataset = remove_duplicates(dataset)
     assert deduplicate_dataset.shape == (39, 5)
+
+
+def test_balance_multilabel_dataset_with_quantile_zero():
+    dataset = pd.read_csv(os.path.join(CSV_FILEPATH, "dataset_build/mock_data_build.csv"), sep="\t")
+
+    balanced_dataset = balance_multilabel_dataset(dataset, quantile=0.0)
+    label_counts = balanced_dataset.iloc[:, 1:].sum()
+
+    expected_result = pd.Series(data=[7, 17, 17, 17], index=["SDG1", "SDG2", "SDG3", "SDG4"])
+    assert (label_counts == expected_result).all()
+
+
+def test_balance_multilabel_dataset_with_median():
+    dataset = pd.read_csv(os.path.join(CSV_FILEPATH, "dataset_build/mock_data_build.csv"), sep="\t")
+
+    balanced_dataset = balance_multilabel_dataset(dataset, quantile=0.5)
+    label_counts = balanced_dataset.iloc[:, 1:].sum()
+
+    expected_result = pd.Series(data=[3, 18, 16, 15], index=["SDG1", "SDG2", "SDG3", "SDG4"])
+    assert (label_counts == expected_result).all()
+
+
+def test_balance_multilabel_dataset_with_max_data():
+    dataset = pd.read_csv(os.path.join(CSV_FILEPATH, "dataset_build/mock_data_build.csv"), sep="\t")
+
+    balanced_dataset = balance_multilabel_dataset(dataset, quantile=1.0)
+    label_counts = balanced_dataset.iloc[:, 1:].sum()
+
+    expected_result = pd.Series(data=[8, 21, 18, 17], index=["SDG1", "SDG2", "SDG3", "SDG4"])
+    assert (label_counts == expected_result).all()
+
 
