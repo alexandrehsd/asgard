99a9fdf76751c3c4b5dce1928631f61215607f99
Alexandre Henrique <alexandre.hsd@gmail.com>
2023-04-14 21:04:20 -0300

    Update tensorflow load method

--- asgard/utils/data_loader.py
+++ asgard/utils/data_loader.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 
 # load pre-saved tensorflow datasets
 def load_datasets(datapath):
-    train_set = tf.data.experimental.load(os.path.join(datapath, "train_set"))
-    valid_set = tf.data.experimental.load(os.path.join(datapath, "valid_set"))
-    test_set = tf.data.experimental.load(os.path.join(datapath, "test_set"))
+    train_set = tf.data.Dataset.load(os.path.join(datapath, "train_set"))
+    valid_set = tf.data.Dataset.load(os.path.join(datapath, "valid_set"))
+    test_set = tf.data.Dataset.load(os.path.join(datapath, "test_set"))
 
     return train_set, valid_set, test_set
